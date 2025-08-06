08d3b6b999baa2de36313ea2afc1db4d00eb0d0b
Salem Albarudy <saljan93@live.com>
2025-08-06 20:28:07 +0200

    solve #1
    * draft
    
    * add features
    
    * remove trash
    
    * solve #1
    
    * update side bar
    
    * ProtectedRoute
    
    * solve #1
    
    ---------
    
    Co-authored-by: salem <salem>

---
+++ src/features/clients/pages/ClientsList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CCard, CCardBody, CCardHeader } from '@coreui/react'
+
+const ClientsList = () => {
+  return (
+    <CCard className="mb-4">
+      <CCardHeader>ClientsList</CCardHeader>
+      <CCardBody>ClientsList</CCardBody>
+    </CCard>
+  )
+}
+
+export default ClientsList
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CButton
+} from '@coreui/react'
+import { Link } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <>
+      <CRow>
+        <CCol xs={12}>
+          <CCard className="mb-4">
+            <CCardHeader>
+              <strong>Dashboard</strong>
+            </CCardHeader>
+            <CCardBody>
+              Welcome to Melon Business Dashboard<br />
+
+              <Link to="/dashboard">Dashboard</Link><br />
+              <Link to="/appointments">appointments</Link><br />
+              <Link to="/sandbox">sandbox</Link><br />
+
+              <Link to="/schedule">schedule</Link><br />
+              <Link to="/staff">staff</Link><br />
+              <Link to="/clients">clients</Link><br />
+              <Link to="/services">services</Link><br />
+              <Link to="/analytics">analytics</Link><br />
+              <Link to="/settings">settings</Link><br />
+
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  )
+}
+
+export default Dashboard
