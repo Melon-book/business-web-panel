17d6ff8e9cec2be3ea0721af5b52beb4d44f8798
salem <salem>
2025-08-06 20:10:44 +0200

    ProtectedRoute

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -131,9 +131,10 @@ const AppHeader = () => {
           <AppHeaderDropdown />
         </CHeaderNav>
       </CContainer>
-      <CContainer className="px-4" fluid>
+      {/* TODO ... do we need it? */}
+      {/* <CContainer className="px-4" fluid>
         <AppBreadcrumb />
-      </CContainer>
+      </CContainer> */}
     </CHeader>
   )
 }
